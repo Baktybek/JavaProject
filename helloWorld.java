e2174e58232003412220bb95aeb13c00227afa09
Baktybek Tazhibaev <baktybektazhibaev@gmail.com>
2016-11-24 18:54:42 -0600

    Create helloWorld.java

---
+++ helloWorld.java
@@ -0,0 +1,17 @@
+import java.Scanner;
+package javaapplication1;
+
+/**
+ *
+ * @Baktybek Tazhibaev
+ */
+public class JavaApplication1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=0; i<n; i++){
+        System.out.println("Hello World1");
+    }
+    }
+}
