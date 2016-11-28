2c87b0060fe67f68342cf10c788c4fc37fbae66c
Baktybek Tazhibaev <baktybektazhibaev@gmail.com>
2016-11-27 23:51:30 -0600

    Update randomNumber.java

--- randomNumber.java
+++ randomNumber.java
@@ -6,20 +6,25 @@ public class randomNumber {
     public static void main(String[] args){
       //Random creates random numbers
         Random rand = new Random();
+        System.out.println("Please enter any decimal number: ");
         Scanner scan = new Scanner(System.in);
         int n = scan.nextInt();
         int[] array = new int[n];
-        
+        int count = 0;
         
         
         for(int i = 0; i < n; i++){
           //Each time when random number within 100 is being created 
           //it will be assigned to the according index of an array
             array[i] = rand.nextInt(100);
-            System.out.print(i);
+            System.out.print(i+1);
             System.out.print("-"+array[i]+" ");
             System.out.println("");
+          //"count" will keep adding each randomly generated number and add it to the one before
+            count = count + array[i];
         }
+        //This will print out the average of randomly created elements of an array
+        System.out.println(count/n);
       
     }
     
