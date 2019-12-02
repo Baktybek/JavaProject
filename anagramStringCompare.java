267d0bfbefa8f050c0d5d80f035570d310ecca43
Baktybek Tazhibaev <baktybektazhibaev@gmail.com>
2019-12-01 23:58:42 -0600

    Update anagramStringCompare.java

--- anagramStringCompare.java
+++ anagramStringCompare.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class anagramStringCompare {
     public static void main(String[] args) {
@@ -7,26 +7,28 @@ public class anagramStringCompare {
         String s1 = input.nextLine();
         System.out.println("Enter the second string: ");
         String s2 = input.nextLine();
-        String newEmpty = "";
 
-        if(s1.equalsIgnoreCase(s2)){
-            System.out.println("It is an anagram.");
+        if(s1.length() != s2.length()){
+            System.out.println("Not an anagram.");
         }
 
         if (s1.length() == s2.length()) {
             for (int i = 0; i <= s1.length() - 1; i++) {
                 for (int x = 0; x <= s2.length() - 1; x++) {
                     if (s1.charAt(i) == s2.charAt(x)){
-                        newEmpty += s2.charAt(x);
+                        s2 = s2.substring(0, x) + s2.substring(x+1);//concatenate both parts of s2 after removing the matching character
+                        break;
                     }
                 }
             }
-            if(newEmpty.equalsIgnoreCase(s1)){
+            //below here i will check if the s2 is empty after removing found(matched) characters
+            if(s2.isEmpty()){
+                //if it is empty that means all characters exist in both s1 & s2
                 System.out.println("It is an anagram.");
             }
-        }
-        else{
-            System.out.println("Not an anagram.");
+            else{
+                System.out.println("Not an anagram.");
+            }
         }
     }
 }
