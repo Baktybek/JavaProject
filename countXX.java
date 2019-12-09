2b9c62f98d6bcca44ce371b1b6c011a673fb2212
Baktybek Tazhibaev <baktybektazhibaev@gmail.com>
2019-12-09 16:38:34 -0600

    Update countXX.java

--- countXX.java
+++ countXX.java
@@ -13,3 +13,16 @@ int countXX(String str) {
   }
   return count;
 }
+.
+.
+.
+  //IT'S SAME WITH THE ABOVE CODE
+int countXX(String str) {
+  int count = 0;
+  for (int i = 0; i < str.length()-1; i++) {
+    if (str.substring(i, i+2).equals("xx")){
+      count++;
+    } 
+  }
+  return count;
+}
